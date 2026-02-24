8ad845a13ff93edb18fc3dd228291c1a60e2652b
lebrauhnjames <iblamewirtz@gmail.com>
2026-02-24 14:10:44 -0300

    Add player configuration for UserId 442076114

--- chat/PlayerConfigs.lua
+++ chat/PlayerConfigs.lua
@@ -4,6 +4,10 @@ return {
         Username = "noah",
         PFP = "rbxassetid://12508777300"
     },
+        [442076114] = { 
+        Username = "noah",
+        PFP = "rbxassetid://12508777300"
+    },
     [7295784691] = {
         Username = "flyest",
         PFP = "rbxassetid://139178155003800"
